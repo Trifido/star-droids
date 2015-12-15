e45c6c7cfba9080d82121e50ff8702321af45b01
Nikolai-Yeratel <nikolaigonzalezlopez@gmail.com>
2015-12-15 12:11:29 +0100

    Sonido
    Paquete con la clase Sonido para reproducir MP3 ^^

---
+++ StarDroids/src/Sonido/Sonido.java
@@ -0,0 +1,52 @@
+package Sonido;
+
+import java.io.*;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+/**
+ *
+ * @author Nikolai Giovanni González López
+ */
+
+public class Sonido {
+    private String ruta;
+    private Player player;
+    
+    public Sonido(String file)
+    {
+        ruta = file;
+    }
+    
+    public void reproducirMP3() 
+    {
+        try
+        {
+            InputStream fis = this.getClass().getResourceAsStream(ruta);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            player = new Player(bis);
+        }
+        catch (JavaLayerException e) 
+        {
+            e.printStackTrace();
+        } 
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    player.play();
+                }
+                catch(JavaLayerException e){}
+            }
+        }.start();
+    }
+    
+    public Player getPlayer()
+    {
+        return player;
+    }
+}
+
